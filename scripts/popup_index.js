2278224d95425e30095c2aa956e237ac81a6bb1b
JotaBrandao <1221446@isep.ipp.pt>
2025-06-16 23:43:30 +0100

    update index.html

---
+++ scripts/popup_index.js
@@ -0,0 +1,17 @@
+//Popup Sobre Nós
+function abrirPopupSobre() {
+    document.getElementById("popup-sobre").style.display = "flex";
+}
+
+function fecharPopupSobre() {
+    document.getElementById("popup-sobre").style.display = "none";
+}
+
+//Popup Contactos
+function abrirPopupContactos() {
+    document.getElementById("popup-contactos").style.display = "flex";
+}
+
+function fecharPopupContactos() {
+    document.getElementById("popup-contactos").style.display = "none";
+}

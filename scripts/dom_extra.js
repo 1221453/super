ced26eaa9bf84ca9209f6172b00d8d5198075559
1221453 <1221453@isep.ipp.pt>
2025-06-20 18:21:42 +0100

    dom e userstories atualizados

---
+++ scripts/dom_extra.js
@@ -0,0 +1,7 @@
+
+// dom_extra.js
+
+function mostrarDescricaoProjeto() {
+    const el = document.getElementById("descricaoProjeto");
+    el.style.display = el.style.display === "none" ? "block" : "none";
+}
